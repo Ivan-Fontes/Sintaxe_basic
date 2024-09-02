036e9157ac9254d11ce5b52effbcdf5cfe93d561
Ivan Fontes De Assis Santos <162522430+Ivan-Fontes-de-Assis-Santos@users.noreply.github.com>
2024-09-02 11:24:57 -0300

    Add files via upload

---
+++ Operadores_aritmétricos.py
@@ -0,0 +1,28 @@
+#Importando o Sys para aumentar o limite de strings, inteiros e float:
+import sys
+sys.set_int_max_str_digits(10000)
+
+#Adição:
+print(1+1)
+#Subtração:
+print(10-2)
+#Multiplicação:
+print(4 * 3)
+#Divisões:
+print(12 / 3)
+print( 12 // 3)
+#Modulo:
+print(10 % 3)
+#Exponenciação:
+print(2 ** 3)
+
+prod_1= 1000
+prod_2= 2286
+
+print(prod_1 - prod_2)
+print(prod_2 + prod_1)
+print(prod_1 / prod_2)
+print(prod_2 // prod_1)
+print(prod_1 * prod_2)
+print(prod_2 % prod_1)
+print(prod_1 ** prod_2)
