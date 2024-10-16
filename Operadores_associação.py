fbdc60fa26f5e3534bed7a9ecd77efbd6109773d
Ivan-Fontes <161970251+Ivan-Fontes@users.noreply.github.com>
2024-10-16 18:27:24 -0300

    Add files via upload

---
+++ Operadores_associação.py
@@ -0,0 +1,8 @@
+Curso= "Curso de Python"
+Frutas= ["Banana", "Melão", "Caju"]
+Saques= [1000,400]
+#Este é o Operador de associação "in" ou "em", para verificar se algo está naquela sequencia:
+print("Python" in Curso)
+print("Maça" not in Frutas)
+print(500093 in Saques)
+
